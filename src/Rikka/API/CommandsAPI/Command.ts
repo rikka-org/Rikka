427afebdb23c8c855e35ffcd1ccaba9f5b0460ae
V3L0C1T13S <turtletester20@gmail.com>
2022-03-26 03:56:47 -0400

    Added code injector

--- src/Rikka/API/CommandsAPI/Command.ts
+++
@@ -1,6 +0,0 @@
-export default abstract class Command {
-    abstract name: string;
-    abstract description: string;
-
-    abstract callback(): void;
-}
--- src/Rikka/API/dommod/index.ts
+++
@@ -1,6 +0,0 @@
-// Dom mod that allows sandboxed code to access the DOM, while also preventing access to sensitive elements.
-
-import BaseAPI from "@rikka/API/BaseAPI";
-
-export default class dommod extends BaseAPI {
-}
