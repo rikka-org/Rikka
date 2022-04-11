bebe50dbb33b887fbbc7245e37e26555b92b296b
V3L0C1T13S <turtletester20@gmail.com>
2022-04-11 08:58:16 -0400

    Work on settings API

---
+++ src/Rikka/API/settings/store/constants.ts
@@ -0,0 +1,7 @@
+export namespace RK_SETTINGS_CONSTANTS {
+    export const STORE_LOCATION = `${__dirname}/settings`;
+    export namespace settings_types { 
+        export const SET_SETTING = 'SET_SETTING';
+    };
+
+}
