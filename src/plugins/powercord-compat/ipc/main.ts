edee5c439b2d023059efe81486e7c25c18956641
V3L0C1T13S <turtletester20@gmail.com>
2022-04-19 14:21:10 -0400

    Fix powercord window maximized

--- src/plugins/powercord-compat/ipc/main.ts
+++ src/plugins/powercord-compat/ipc/main.ts
@@ -1,6 +1,5 @@
-import { IPC_Consts } from "@rikka/API/Constants";
-import { ipcMain, ipcRenderer } from "electron";
+import { BrowserWindow, ipcMain, ipcRenderer } from "electron";
 
 if (!ipcMain) throw new Error("IPC not available!");
 
-ipcMain?.handle("POWERCORD_WINDOW_IS_MAXIMIZED", e => ipcRenderer.invoke(IPC_Consts.GET_WINDOW_MAXIMIZED));
+ipcMain.handle("POWERCORD_WINDOW_IS_MAXIMIZED", e => BrowserWindow.fromWebContents(e.sender)?.isMaximized());
