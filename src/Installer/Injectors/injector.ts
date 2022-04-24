f8e4b7bd0cc92acb365734dcef9720b71b8edfa2
V3L0C1T13S <turtletester20@gmail.com>
2022-04-24 18:49:30 -0400

    [Feat] cool colors for installer

--- src/Installer/Injectors/injector.ts
+++ src/Installer/Injectors/injector.ts
@@ -4,6 +4,14 @@ import { join, sep } from "node:path";
 import { GetDiscordInstallPath } from "../Utils/AppFinder";
 import { switchToRoot } from "./su";
 
+const AnsiEscapes = {
+    RESET: '\x1b[0m',
+    BOLD: '\x1b[1m',
+    GREEN: '\x1b[32m',
+    YELLOW: '\x1b[33m',
+    RED: '\x1b[31m'
+};
+
 export async function InjectRikka(branch: string) {
     const discordInstall = GetDiscordInstallPath(branch);
 
@@ -15,20 +23,20 @@ export async function InjectRikka(branch: string) {
 
     /** Failsafe for corrupted installs. */
     if (!existsSync(discordInstall.path)) await mkdir(discordInstall.path, { recursive: true })
-        .catch(e => { throw new Error(`Failed to create directory for Rikka! ${e}`) });
+        .catch(e => { throw new Error(`${AnsiEscapes.BOLD}${AnsiEscapes.RED}Failed to create directory for Rikka! ${e}${AnsiEscapes.RESET}`) });
 
     // Write a file to the Discord installation directory that calls a require() on the Rikka module.
     writeFile(join(discordInstall.path, "Rikka.js"),
         `require(\`${__dirname.replace(RegExp(sep.repeat(2), 'g'), '/')}/../..\`)`
     )
-        .catch(e => { throw new Error(`Failed to write Rikka.js! ${e}`) });
+        .catch(e => { throw new Error(`${AnsiEscapes.BOLD}${AnsiEscapes.RED}Failed to write Rikka.js! ${e}${AnsiEscapes.RESET}`) });
     writeFile(join(discordInstall.path, 'package.json'), JSON.stringify({
         name: 'discord',
         main: 'Rikka.js'
     })
     );
 
-    console.log("Rikka injected successfully!");
+    console.log(`${AnsiEscapes.BOLD}${AnsiEscapes.GREEN}Rikka injected successfully!${AnsiEscapes.RESET}`);
 }
 
 export async function UninjectRikka(branch: string) {
@@ -43,5 +51,5 @@ export async function UninjectRikka(branch: string) {
     await rm(discordInstall.path, { recursive: true })
         .catch(e => { throw new Error(`Failed to delete Rikka.js! ${e}`) });
 
-    console.log("Rikka uninjected successfully!");
+    console.log(`${AnsiEscapes.BOLD}${AnsiEscapes.GREEN}Rikka uninjected successfully!${AnsiEscapes.RESET}`);
 }
